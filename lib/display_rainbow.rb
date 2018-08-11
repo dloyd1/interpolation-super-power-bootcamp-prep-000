d9f6c424c3d25b4767842e1c3cb577f64aecb062
Daniel Loyd <dhloyd@icloud.com>
2018-08-11 16:37:41 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
 my_array = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
 
-my_array[index]
+def display_rainbow('R', 'O', 'Y', 'G', 'B', 'I', 'V')
+  puts "R: #{R}, O: #{O}, Y: #{Y}, G: #{G}, B: #{B}, I: #{I}, V: #{V}"
